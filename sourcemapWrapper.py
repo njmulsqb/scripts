3f74d585600f3607a50e0831e404a5d181be3bbe
Najam Ul Saqib <najamulsaqib@tutamail.com>
2022-05-31 11:38:07 +0500

    Update sourcemapWrapper.py
    remove print statements

--- sourcemapWrapper.py
+++ sourcemapWrapper.py
@@ -2,12 +2,10 @@ import sys
 import subprocess
 import os
 
-print(sys.argv[1])
 parentDir = 'sourceMapsToJS'
 sourceMapURL = sys.argv[1]
 dirName = sourceMapURL.strip(
     'https://').strip('http://').strip('www.').replace('/', '-').replace('.', '-')
-print(dirName)
 if not os.path.exists(parentDir):
     os.mkdir(parentDir)
 os.chdir(parentDir)
